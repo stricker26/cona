cc48663a070c08c86d9e67e7d6f631943fd0502d
xandyuck <xandybreyes@gmail.com>
2018-09-13 16:14:40 +0800

    Webpack

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,15 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Cona 2018') }}
+                <a class="navbar-brand" href="{{ url('/') }}" >
+                    <img src="{{ asset('img/LP_Logo.png')}}">
+                    <!--{{ config('app.name', 'Cona 2018') }}-->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
