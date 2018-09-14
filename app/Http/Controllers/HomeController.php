6b62585ed133dcb9c33ac45b5459068b4277daa9
BCD Pinpoint <rjhonnas@gmail.com>
2018-09-14 09:28:44 +0800

    Remove login from candidate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,16 +12,6 @@ use DB;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
