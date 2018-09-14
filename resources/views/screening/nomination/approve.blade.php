b76248e2a381667d8924dba4bb6647618e6f7c3d
xandyuck <xandybreyes@gmail.com>
2018-09-14 13:59:27 +0800

    HQ Screening

---
+++ resources/views/screening/nomination/approve.blade.php
@@ -0,0 +1,22 @@
+@extends('dashboard.layouts.master')
+
+@section('title','Nomination')
+
+@section('styles')
+@stop
+
+@section('sub-page','Approved Nominees')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <span>This is the header - Approved</span>
+    </div>
+    <div class="card-body">
+        <span>This is the body</span>
+    </div>
+</div>
+@stop
+
+@section('scripts')
+@stop
---
+++ resources/views/screening/screening.blade.php
@@ -0,0 +1,22 @@
+@extends('dashboard.layouts.master')
+
+@section('title','HQ Screening')
+
+@section('styles')
+@stop
+
+@section('sub-page','HQ Screening')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <span>This is the header</span>
+    </div>
+    <div class="card-body">
+        <span>This is the body</span>
+    </div>
+</div>
+@stop
+
+@section('scripts')
+@stop
