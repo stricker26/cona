f48d9284c0e3a8c773e0f0d141e26fe554f0e4f3
xandyuck <xandybreyes@gmail.com>
2018-09-18 17:04:44 +0800

    Tables

--- public/js/screening/tableScreening.js
+++ public/js/screening/tableScreening.js
@@ -4,6 +4,7 @@ $(document).ready( function () {
     	var e = $(this).find(".code").html();
     	var name = $(this).find(".description").html();
     	ajaxGet(e, name);
+		
 	});
 
 	$('.bcrumbs').on('click', 'a', function(e) {
@@ -17,15 +18,6 @@ $(document).ready( function () {
 		}
 	});
 
-    $('#tableGeo').DataTable( {
-    	"bLengthChange": false,
-    	"bInfo": false,
-    	"ordering": false,
-    	"searching": false,
-    	"pageLength": 20,
-    	"paging": false
-    });
-
 	$('#tableGeo').delegate('tbody > tr', 'mouseenter', function () {
 		$(this).css({'font-weight': 'bold',
 					'cursor': 'pointer'});
@@ -34,43 +26,126 @@ $(document).ready( function () {
 	$('#tableGeo').delegate('tbody > tr', 'mouseleave', function () {
 		$(this).css('font-weight', 'normal');
 	});
-		
-});
 
-function loadTable(data) {
-	var keys = Object.keys(data);
-	var y = keys.length - 1;
-	console.log(data);
-	$('tbody').html('');
-	for (var x=keys[0]; x <= keys[y]; x++) {
-		$('tbody').append(`
-				<tr>
-					<td class="code">` + data[x].code + `</td>
-					<td class="description">` + data[x].description + `</td>
-					<td>0</td>
-					<td>0</td>
-					<td>0</td>
-					<td>Lorem Ipsum</td>
-				</tr>
-			`);
+	$('th').click(function() {
+		if ($(this).children('i').hasClass('x')) {
+			if ($(this).children('i').hasClass('fa-sort-amount-asc')) {
+				$(this).children('i').removeClass('fa-sort-amount-asc');
+				$(this).children('i').addClass('fa-sort-amount-desc');
+			} else {
+				$(this).children('i').removeClass('fa-sort-amount-desc');
+				$(this).children('i').addClass('fa-sort-amount-asc');
+			}
+		}
+		$(this).children('i').addClass('x');
+		$(this).siblings().children('i').removeClass('x');
+		$(this).siblings().children('i').removeClass('fa-sort-amount-desc');
+		$(this).siblings().children('i').addClass('fa-sort-amount-asc');
+	});
+
+	loadPagination();
+
+	function loadTable(e, data) {
+		var keys = Object.keys(data);
+		/*
+		var y = keys.length - 1;
+		var arr = [];
+		var z = 0;
+		console.log('length: ' + keys.length);
+		for (var x=0; x < keys.length; x++) {
+			z = keys[x];
+			arr[x] = data[z];
+		}
+		arr.sort(SortByDescription);
+		*/
+		$('tbody').html('');
+		for (var x=keys[0]; x <= keys[y]; x++) {
+			$('tbody').append(`
+					<tr class='item'>
+						<td class="code">` + data[x].code + `</td>
+						<td class="description">` + data[x].description + `</td>
+						<td>0</td>
+						<td>0</td>
+						<td>0</td>
+						<td>Lorem Ipsum</td>
+					</tr>
+				`);
+			loadPagination();
+		}
 	}
 
+	function ajaxGet(e, name) {
+		$.ajax({
+			method: 'GET',
+			url: '/screening/' + e,
+			success:function(data)  
+	    	{
+	    		if (data == '') {
+	    		}
+	    		else  {
+	    			if (name != undefined) {
+	    				$('.bcrumbs').append('<p>/</p> <a href="" id="' + e + '">' + name + '</a>');
+	    			}
+	    			loadTable(e, data);
+	    		}
+	    	} 
+		});
+	}
+});
+
+function searchData() {
+	// Declare variables 
+	var input, filter, table, tr, td, i;
+	input = document.getElementById("searchBar");
+  	filter = input.value.toUpperCase();
+  	table = document.getElementById("tableGeo");
+  	tr = table.getElementsByTagName("tr");
+
+  	// Loop through all table rows, and hide those who don't match the search query
+  	for (i = 0; i < tr.length; i++) {
+    	td1 = tr[i].getElementsByTagName("td")[0];
+    	td2 = tr[i].getElementsByTagName("td")[1];
+    	td5 = tr[i].getElementsByTagName("td")[5];
+    	if (td1 || td2 || td5) {
+      		if (td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1 || td5.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        		tr[i].style.display = "";
+      		} else {
+        		tr[i].style.display = "none";
+      		}
+    	} 
+  	}
 }
 
-function ajaxGet(e, name) {
-	$.ajax({
-		method: 'GET',
-		url: '/screening/' + e,
-		success:function(data)  
-    	{
-    		console.log(data);
-    		if (data == '') {
-    		} else  {
-    			if (name != undefined) {
-    				$('.bcrumbs').append('<p>/</p> <a href="" id="' + e + '">' + name + '</a>');
-    			}
-    			loadTable(data);
-    		}
-    	} 
-	});
+function loadPagination() {
+    var rowsShown = 10;
+    var rowsTotal = $('#tableGeo tbody tr').length;
+    var numPages = rowsTotal/rowsShown;
+    $('#nav').html('');
+    for(i = 0;i < numPages;i++) {
+        var pageNum = i + 1;
+        $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+    }
+    $('#tableGeo tbody tr').hide();
+    $('#tableGeo tbody tr').slice(0, rowsShown).show();
+    $('#nav a:first').addClass('active');
+    $('#nav a').bind('click', function(){
+        $('#nav a').removeClass('active');
+        $(this).addClass('active');
+        var currPage = $(this).attr('rel');
+        var startItem = currPage * rowsShown;
+        var endItem = startItem + rowsShown;
+        $('#tableGeo tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+        css('display','table-row').animate({opacity:1}, 300);
+    });
 }
+
+
+/***
+function SortByID(x,y) {
+	return x.ID - y.ID; 
+}
+
+function SortByDescription(x,y) {
+    return ((x.Description == y.Description) ? 0 : ((x.Description > y.Description) ? 1 : -1 ));
+}
+***/
--- resources/views/dashboard/screening/screening.blade.php
+++ resources/views/dashboard/screening/screening.blade.php
@@ -7,6 +7,7 @@
 @section('styles')
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
 	<link rel="stylesheet" href="{{ asset('css/table-screening.css') }}">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 @stop
 
 @section('sub-page','HQ Screening')
@@ -14,21 +15,22 @@
 @section('content')
 	<div class="container">
 		<div class="bcrumbs row">
-				<a href="" id="regionNum">PH</a>
+			<a href="" id="regionNum">PH</a>
 		</div>
+		<input type="text" id="searchBar" class="form-control pull-right" onkeyup="searchData()" placeholder="SEARCH">
 		<table id="tableGeo" class="table table-striped table-bordered">
 			<thead>
 				<tr>
-					<th>#</th>
-					<th class="colTitle">Name</th>
-					<th>Pending</th>
-					<th>Approved</th>
-					<th>Rejected</th>
-					<th>LEC</th>
+					<th class="colNum" onclick="w3.sortHTML('#tableGeo', '.item', '.code')" style="cursor:pointer">#<i class="fa fa-sort-amount-asc pull-right"></i></th>
+					<th class="colTitle" onclick="w3.sortHTML('#tableGeo', '.item', '.description')" style="cursor:pointer">Name<i class="fa fa-sort-amount-asc pull-right"></i></th>
+					<th class="colPending" onclick="w3.sortHTML('#tableGeo', '.item', 'td:nth-child(3)')" style="cursor:pointer">Pending<i class="fa fa-sort-amount-asc pull-right"></i></th>
+					<th class="colApproved" onclick="w3.sortHTML('#tableGeo', '.item', 'td:nth-child(4)')" style="cursor:pointer">Approved<i class="fa fa-sort-amount-asc pull-right"></i></th>
+					<th class="colRejected" onclick="w3.sortHTML('#tableGeo', '.item', 'td:nth-child(5)')" style="cursor:pointer">Rejected<i class="fa fa-sort-amount-asc pull-right"></i></th>
+					<th class="colLEC" onclick="w3.sortHTML('#tableGeo', '.item', 'td:nth-child(6)')" style="cursor:pointer">LEC<i class="fa fa-sort-amount-asc pull-right"></i></th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
+				<tr class="item">
 					<td class="code">13</td>
 					<td class="description">NCR</td>
 					<td>0</td>
@@ -36,7 +38,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">01</td>
 					<td class="description">ILOCOS REGION</td>
 					<td>0</td>
@@ -44,7 +46,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">02</td>
 					<td class="description">CAGAYAN VALLEY</td>
 					<td>0</td>
@@ -52,7 +54,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">03</td>
 					<td class="description">CENTRAL LUZON</td>
 					<td>0</td>
@@ -60,7 +62,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">04</td>
 					<td class="description">CALABARZON</td>
 					<td>0</td>
@@ -68,7 +70,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">17</td>
 					<td class="description">SOUTHERN TAGALOG REGION</td>
 					<td>0</td>
@@ -76,7 +78,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">05</td>
 					<td class="description">BICOL REGION</td>
 					<td>0</td>
@@ -84,7 +86,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">06</td>
 					<td class="description">WESTERN VISAYAS</td>
 					<td>0</td>
@@ -92,7 +94,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">07</td>
 					<td class="description">CENTRAL VISAYAS</td>
 					<td>0</td>
@@ -100,7 +102,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">08</td>
 					<td class="description">EASTERN VISAYAS</td>
 					<td>0</td>
@@ -108,7 +110,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">09</td>
 					<td class="description">ZAMBOANGA PENINSULA</td>
 					<td>0</td>
@@ -116,7 +118,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">10</td>
 					<td class="description">NORTHERN MINDANAO</td>
 					<td>0</td>
@@ -124,7 +126,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">11</td>
 					<td class="description">DAVAO REGION</td>
 					<td>0</td>
@@ -132,7 +134,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">12</td>
 					<td class="description">SOCCSKSARGEN</td>
 					<td>0</td>
@@ -140,7 +142,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">16</td>
 					<td class="description">CARAGA REGION</td>
 					<td>0</td>
@@ -148,7 +150,7 @@
 					<td>0</td>
 					<td>Lorem Ipsum</td>
 				</tr>
-				<tr>
+				<tr class="item">
 					<td class="code">15</td>
 					<td class="description">ARMM</td>
 					<td>0</td>
@@ -158,12 +160,14 @@
 				</tr>
 			</tbody>
 		</table>
+		<div id="nav"></div>
 	</div>
 @stop
 
 @section('scripts')
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+	<script src="https://www.w3schools.com/lib/w3.js"></script>
 	<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 	<script src="{{asset('js/screening/tableScreening.js')}}" ></script>
 @stop
