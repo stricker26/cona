73c896d58797390a9073bf5b4f4fc43dfff85b46
xandyuck <xandybreyes@gmail.com>
2018-10-03 17:56:42 +0800

    Provincial HUC count fixed

--- app/Http/Controllers/LECController.php
+++ app/Http/Controllers/LECController.php
@@ -46,7 +46,7 @@ class LECController extends Controller
 
         $data = DB::table('huc')
             ->join('province as p', 'huc.province_code', '=', 'p.province_code')
-            ->select('huc.*', DB::raw('(SELECT count(signed_by_lp) FROM candidates WHERE city_id = huc.city AND signed_by_lec = 0 AND (province_id = huc.parent_province_code OR province_id = huc.province_code)) AS pending, (SELECT count(signed_by_lp) FROM candidates WHERE city_id = huc.city AND signed_by_lec = 1 AND (province_id = huc.parent_province_code OR province_id = huc.province_code)) AS approved, (SELECT count(signed_by_lp) FROM candidates WHERE city_id = huc.city AND signed_by_lec = 2 AND (province_id = huc.parent_province_code OR province_id = huc.province_code)) AS rejected, (SELECT name FROM lec WHERE id = p.lec AND p.province_code = huc.province_code) AS assigned_lec'))
+            ->select('huc.*', DB::raw('(SELECT count(signed_by_lp) FROM candidates WHERE signed_by_lec = 0 AND ((province_id = huc.parent_province_code AND city_id = huc.city) OR province_id = huc.province_code)) AS pending, (SELECT count(signed_by_lp) FROM candidates WHERE signed_by_lec = 1 AND ((province_id = huc.parent_province_code AND city_id = huc.city) OR province_id = huc.province_code)) AS approved, (SELECT count(signed_by_lp) FROM candidates WHERE signed_by_lec = 2 AND ((province_id = huc.parent_province_code AND city_id = huc.city) OR province_id = huc.province_code)) AS rejected, (SELECT name FROM lec WHERE id = p.lec AND p.province_code = huc.province_code) AS assigned_lec'))
             ->where('huc.parent_province_code', '=', $code)
             ->where('huc.lec', '=', $lecId)
             ->distinct('huc.id')
