0d82af17d14bdbb42fd3f41e3cbf9f3b5d2fcf02
xandyuck <xandybreyes@gmail.com>
2018-10-09 14:43:07 +0800

    Update resources/views/lec/screening/profile.blade.php

--- resources/views/lec/screening/profile.blade.php
+++ resources/views/lec/screening/profile.blade.php
@@ -468,7 +468,7 @@
 				<button type="button" class="btn btn-secondary" id="edit_btn">Edit</button>
 			</div>
 			@if ($candidate->signed_by_lec == 0)
-				@if ((($candidate->candidate_for == 'City Mayor' || $candidate->candidate_for == 'Congressman' || $candidate->candidate_for == 'HUC Congressman' || $candidate->candidate_for == 'Governor') && $lec->id == '2018000' && strpos($province->lec, '2018000') === false ) || ($lec->id == '2018000' && strpos($province->lec, '2018000') !== false) || ($lec->id != '2018000' && ($candidate->candidate_for != 'City Mayor' && $candidate->candidate_for != 'Congressman' && $candidate->candidate_for != 'HUC Congressman' && $candidate->candidate_for != 'Governor')) )
+				@if ((($candidate->candidate_for == 'City Mayor' || $candidate->candidate_for == 'Congressman' || $candidate->candidate_for == 'HUC Congressman' || $candidate->candidate_for == 'Governor') && $lec->id == '2018000' && strpos($province->lec, '2018000') === false ) || ($lec->id == '2018000' && strpos($province->lec, '2018000') !== false) || ($lec->id != '2018000' && ($candidate->candidate_for != 'City Mayor' && $candidate->candidate_for != 'Congressman' && $candidate->candidate_for != 'HUC Congressman' && $candidate->candidate_for != 'Governor') && strpos($province->lec, '2018000') === false) )
 					<div class="d-inline pr-2">
 						<button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalApprove" id="approve_btn_1">Approve</button>
 					</div>
